8908054c89b81a3e3370d69854f3b6c802394ad1
Bhaskara Ida Bagus <gusbhas@gmail.com>
2017-06-05 06:34:11 +0700

    Add Azure Mobile App Service

--- Xamarin.Wallet/MainActivity.cs
+++ Xamarin.Wallet/MainActivity.cs
@@ -11,6 +11,13 @@ namespace Xamarin.Wallet
         {
             base.OnCreate(bundle);
 
+            // Initialization for Azure Mobile Apps
+            Microsoft.WindowsAzure.MobileServices.CurrentPlatform.Init();
+            // This MobileServiceClient has been configured to communicate with the Azure Mobile App and
+            // Azure Gateway using the application url. You're all set to start working with your Mobile App!
+            Microsoft.WindowsAzure.MobileServices.MobileServiceClient vctWallClient = new Microsoft.WindowsAzure.MobileServices.MobileServiceClient(
+            "http://vctwall.azurewebsites.net");
+
             // Set our view from the "main" layout resource
             // SetContentView (Resource.Layout.Main);
         }
